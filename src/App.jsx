a75666f455c30a26e6a55c1cea47b1535771cef8
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-12-11 15:52:40 +0100

    feat: array filtrato

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Implementare la homepage",
+    priority: 1,
+    estimatedTime: 120,
+    state: "completed"
+  },
+  {
+    id: 2,
+    title: "Sviluppare il menu di navigazione",
+    priority: 2,
+    estimatedTime: 60,
+    state: "completed"
+  },
+  {
+    id: 3,
+    title: "Creare il footer del sito",
+    priority: 3,
+    estimatedTime: 30,
+    state: "completed"
+  },
+  {
+    id: 4,
+    title: "Ottimizzare le performance",
+    priority: 1,
+    estimatedTime: 180,
+    state: "completed"
+  },
+  {
+    id: 5,
+    title: "Scrivere test per i componenti",
+    priority: 2,
+    estimatedTime: 90,
+    state: "completed"
+  },
+  {
+    id: 6,
+    title: "Implementare la pagina dei contatti",
+    priority: 3,
+    estimatedTime: 60,
+    state: "completed"
+  },
+  {
+    id: 7,
+    title: "Aggiungere animazioni CSS",
+    priority: 2,
+    estimatedTime: 30,
+    state: "backlog"
+  },
+  {
+    id: 8,
+    title: "Integrare l'API di autenticazione",
+    priority: 1,
+    estimatedTime: 120,
+    state: "in_progress"
+  },
+  {
+    id: 9,
+    title: "Aggiornare la documentazione",
+    priority: 3,
+    estimatedTime: 60,
+    state: "backlog"
+  },
+  {
+    id: 10,
+    title: "Rifattorizzare il codice CSS",
+    priority: 2,
+    estimatedTime: 90,
+    state: "in_progress"
+  }
+];
+
+const filteredElem = tasks.filter((curCompleted) => curCompleted.state === "completed");
+const otherElem = tasks.filter((curOther) => curOther.state != "completed");
+
+console.log(filteredElem);
+console.log(otherElem);
+
+
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Task Manager</h1>
+
+    <h3>Current Tasks (4)</h3>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+
+    <hr/>
+    <h3>Completed Tasks (6)</h3>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    </>
+  )
+}
+
+export default App
